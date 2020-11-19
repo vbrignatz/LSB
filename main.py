519cae31c4999e25827d636c7fd3d4409706d865
vbrignatz <vincentbrignatz@gmail.com>
2020-11-19 11:09:13 +0100

    [doc] added docstrings

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ parser.add_argument('-m', "--message", type=str,  required=True,
 args = parser.parse_args()
 
 def split_number(n):
-    """ split a byte in two 4 bits parts """
+    """ Split a byte into two 4 bits parts 
+        (int) ~> (int, int)
+
+        >>> split_number(0b10100101)
+        (0b1010, 0b0101)
+    """
     if n > 0b11111111:
         raise AttributeError("The number given is not a Byte")
     h_part = (n & 0b11110000) >> 4
@@ -18,14 +23,32 @@ def split_number(n):
     return h_part, l_part
 
 def merge_number(h_part, l_part):
+    """ Merge two 4 bits parts into a byte
+        (int, int) ~> (int)
+
+        >>> merge_number(0b1010, 0b0101)
+        0b10100101
+    """
     if h_part > 0b1111 or l_part > 0b1111:
         raise AttributeError("The parts given is not a 4 bits digits")
     return (h_part << 4) + l_part
 
-def replace_bits(number, bits):
+def replace_lsb(number, bits):
+    """ Replace the 4 LSB by 4 bits
+        (int, int) ~> (int)
+
+        >>> replace_lsb(0b10100101, 0b1100)
+        0b10101100
+    """
     return (number & 0b11110000) + bits
 
 def create_palette(rgb_img):
+    """ Create a list of all unique element from rgb_img
+        (List<List<Tuple<int>>>) ~> (List<Tuple<int>>)
+
+        >>> create_palette([[(1, 2, 3), (4, 5, 6)], [(1, 2, 3), (7, 8, 9)]])
+        [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+    """
     palette = []
     for row in rgb_img:
         for px in row:
@@ -34,14 +57,29 @@ def create_palette(rgb_img):
     return palette
 
 def int_to_rgb_img(int_img, palette):
+    """ Put the element of palette in int_img by using the indice in int_img.
+        This is usefull to convert the palette system of pypng to a rgb matrix.
+        (List<List<int>>) ~> (List<List<Tuple<int>>>)
+
+        >>> int_to_rgb_img([[0, 1], [0, 2]], [(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+        [[(1, 2, 3), (4, 5, 6)], [(1, 2, 3), (7, 8, 9)]]
+    """
     rgb_img = []
     for r in int_img:
         rgb_img.append([])
         for px in r:
-            rgb_img[-1].append(infos["palette"][px])
+            rgb_img[-1].append(palette[px])
     return rgb_img
 
 def rgb_to_int_img(rgb_img, palette):
+    """ Replace the rgb tuples of rgb_img by their indices in the palette.
+        This is usefull to convert a rgb matrix to the palette system of pypng.
+        (List<List<Tuple<int>>>) ~> (List<List<int>>)
+
+        >>> rgb_to_int_img([[(1, 2, 3), (4, 5, 6)], [(1, 2, 3), (7, 8, 9)]],
+                            [(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+        [[0, 1], [0, 2]]
+    """
     int_img = []
     for row in rgb_img:
         int_img.append([])
@@ -50,6 +88,12 @@ def rgb_to_int_img(rgb_img, palette):
     return int_img
 
 def hide_message(rgb_img, message):
+    """ 
+        (List<List<Tuple<int>>>) ~> (List<List<Tuple<int>>>)
+
+        >>> hide_message([[(1, 2, 3, 0), (4, 5, 6, 0)], [(1, 2, 3, 0), (7, 8, 9, 0)]], "abc")
+        [[(6, 1, 3, 0), (6, 2, 6, 0)], [(6, 3, 3, 0), (7, 8, 9, 0)]]
+    """
     new_rgb_img = []
     i = 0
     for row in rgb_img:
@@ -58,8 +102,8 @@ def hide_message(rgb_img, message):
             if i < len(message):
                 h, l = split_number(ord(message[i]))
                 new_px = (
-                            replace_bits(px[0], h),
-                            replace_bits(px[1], l),
+                            replace_lsb(px[0], h),
+                            replace_lsb(px[1], l),
                             px[2],
                             px[3]
                         )
