59358c007694592aed9dfc0a5949f41ccef72272
vbrignatz <vincentbrignatz@gmail.com>
2020-11-20 16:49:22 +0100

    feat: optimised code, added diff pictures

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def split_number(n):
     l_part =  n & 0b1111
     return h_part, l_part
 
-def merge_number(h_part, l_part):
-    """ Merge two 4 bits parts into a byte
+def concat_number(h_part, l_part):
+    """ Concatenate two 4 bits numbers into a byte.
         (int, int) ~> (int)
 
-        >>> merge_number(0b1010, 0b0101)
+        >>> concat_number(0b1010, 0b0101)
         0b10100101
     """
     if h_part > 0b1111 or l_part > 0b1111:
@@ -66,103 +66,61 @@ def replace_lsb(number, bits):
     """
     return (number & 0b11110000) + bits
 
-def create_palette(rgb_img):
-    """ Create a list of all unique element from rgb_img
-        (List<List<Tuple<int>>>) ~> (List<Tuple<int>>)
-
-        >>> create_palette([[(1, 2, 3), (4, 5, 6)], [(1, 2, 3), (7, 8, 9)]])
-        [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
-    """
-    palette = []
-    for row in rgb_img:
-        for px in row:
-            if px not in palette:
-                palette.append(px)
-    return palette
-
-def int_to_rgb_img(int_img, palette):
-    """ Put the element of palette in int_img by using the indice in int_img.
+def palette_to_rgba(idx_img, palette):
+    """ Put the element of palette in idx_img by using the indice in idx_img.
         This is usefull to convert the palette system of pypng to a rgb matrix.
         (List<List<int>>) ~> (List<List<Tuple<int>>>)
 
-        >>> int_to_rgb_img([[0, 1], [0, 2]], [(1, 2, 3), (4, 5, 6), (7, 8, 9)])
-        [[(1, 2, 3), (4, 5, 6)], [(1, 2, 3), (7, 8, 9)]]
+        >>> palette_to_rgba([[0, 1], [0, 2]], [(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+        [[1, 2, 3,  4, 5, 6], [1, 2, 3,  7, 8, 9]]
     """
-    rgb_img = []
-    for r in int_img:
-        rgb_img.append([])
+    rgba_img = []
+    for r in idx_img:
+        rgba_img.append([])
         for px in r:
-            rgb_img[-1].append(palette[px])
-    return rgb_img
+            rgba_img[-1].extend(palette[px])
+    return rgba_img
 
-def rgb_to_int_img(rgb_img, palette):
-    """ Replace the rgb tuples of rgb_img by their indices in the palette.
-        This is usefull to convert a rgb matrix to the palette system of pypng.
-        (List<List<Tuple<int>>>) ~> (List<List<int>>)
-
-        >>> rgb_to_int_img([[(1, 2, 3), (4, 5, 6)], [(1, 2, 3), (7, 8, 9)]],
-                            [(1, 2, 3), (4, 5, 6), (7, 8, 9)])
-        [[0, 1], [0, 2]]
-    """
-    int_img = []
-    for row in rgb_img:
-        int_img.append([])
-        for px in row:
-            int_img[-1].append(palette.index(px))
-    return int_img
-
-def hide_message(rgb_img, message):
+def hide_message(rgba_img, message):
     """ Write the lsb from R and G channel of the picture to hide the message.
         (List<List<Tuple<int>>>, str) ~> (List<List<Tuple<int>>>)
 
-        >>> hide_message([[(1, 2, 3, 0), (4, 5, 6, 0)], [(1, 2, 3, 0), (7, 8, 9, 0)]], "abc")
-        [[(6, 1, 3, 0), (6, 2, 6, 0)], [(6, 3, 3, 0), (7, 8, 9, 0)]]
+        >>> hide_message([[1, 2, 3, 0, 4, 5, 6, 0], [1, 2, 3, 0, 7, 8, 9, 0]], "abc")
+        [[6, 1, 3, 0, 6, 2, 6, 0], [6, 3, 3, 0, 7, 8, 9, 0]]
     """
-    new_rgb_img = []
-    i = 0
-    for row in rgb_img:
-        new_rgb_img.append([])
-        for px in row:
-            if i < len(message):
-                h, l = split_number(ord(message[i]))
-                new_px = (
-                            replace_lsb(px[0], h),
-                            replace_lsb(px[1], l),
-                            px[2],
-                            px[3]
-                        )
-                new_rgb_img[-1].append(new_px)
-            else:
-                new_rgb_img[-1].append(px)
-            i+=1
-    return new_rgb_img
+    width = int(len(rgba_img[0]) / 4)
+    for k, char in enumerate(message):
+        h, l = split_number(ord(char))
+        j = int(k/width)
+        i = k%width
+        rgba_img[j][i*4] = h
+        rgba_img[j][i*4+1] = l
+    return rgba_img
 
-def find_message(rgb_img):
+def find_message(rgba_img):
     """ Read the lsb from R and G channel, contatenate them to find the message hidden in the picture.
         (List<List<Tuple<int>>>) ~> (str)
 
-        >>> hide_message([[(6, 1, 3, 0), (6, 2, 6, 0)], [(6, 3, 3, 0), (7, 8, 9, 0)]])
+        >>> find_message([[6, 1, 3, 0, 6, 2, 6, 0], [6, 3, 3, 0, 7, 8, 9, 0]])
         "abcx"
     """
     msg = ""
-    for row in rgb_img:
-        for px in row:
-            _, h = split_number(px[0])
-            _, l = split_number(px[1])
-            msg += chr(merge_number(h, l))
+    for row in rgba_img:
+        for i in range(0, len(rgba_img[0])-3, 4):
+            _, h = split_number(row[i])
+            _, l = split_number(row[i+1])
+            msg += chr(concat_number(h, l))
     return msg
 
 if __name__ == "__main__":
     # Read the image
     r=png.Reader(filename=args.filename)
     width, height, rows, infos = r.asRGBA8()
-    int_img = [list(r) for r in rows]
+    rgba_img = [list(r) for r in rows]
     
+    # convert is palette mode
     if "palette" in infos:
-        rgb_img = int_to_rgb_img(int_img, infos["palette"])
-    else:
-        rgb_img = [[(row[i], row[i+1], row[i+2], row[i+3]) for i in range(0, len(row)-3, 4)] for row in int_img]
-
+        rgba_img = palette_to_rgba(rgba_img, infos["palette"])
 
     if args.mode == "write":
         # Check we have a message
@@ -174,24 +132,22 @@ if __name__ == "__main__":
         print(f"Hiding '{args.text}' in {args.output} from image {args.filename}")
 
         # sanity check
-        n_px = len(rgb_img[0]) * len(rgb_img)
+        n_px = width * height
         n_msg = len(args.text)
         if n_px < n_msg:
             raise MemoryError(f"The text ({n_msg} chars) is too fat for the image you have choosen ({n_px} pixels)")
 
         # hide the message
-        new_rgb_img = hide_message(rgb_img, args.text)
+        new_rgba_img = hide_message(rgba_img, args.text)
 
-        # save new image in out.png as a palette rgba
-        palette = create_palette(new_rgb_img)
-        new_int_img = rgb_to_int_img(new_rgb_img, palette)
-        w = png.Writer(len(new_int_img[0]), len(new_int_img), palette=palette, bitdepth=8)
+        # save new image in out.png 
+        w = png.Writer(width, height, bitdepth=8, greyscale=False, alpha=True)
         f = open(args.output, 'wb')
-        w.write(f, new_int_img)
+        w.write(f, new_rgba_img)
 
     elif args.mode == "read":
         # Find the message
-        msg = find_message(rgb_img)
+        msg = find_message(rgba_img)
 
         # Write the message in the ouput file
         with open(args.output, "w") as fout:
